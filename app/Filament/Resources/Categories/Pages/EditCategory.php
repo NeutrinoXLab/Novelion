1fd4a1ac1059f975cead2a41df6e08d324c57ab0
NeutrinoX <dumitrescu.r.codrut@gmail.com>
2026-09-08 15:54:00 +0300

    Prevent category deletion from admin

--- app/Filament/Resources/Categories/Pages/EditCategory.php
+++ app/Filament/Resources/Categories/Pages/EditCategory.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\Categories\Pages;
 
 use App\Filament\Resources\Categories\CategoryResource;
-use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
 
@@ -15,7 +14,6 @@ class EditCategory extends EditRecord
     {
         return [
             ViewAction::make(),
-            DeleteAction::make(),
         ];
     }
 }
--- app/Filament/Resources/Categories/Tables/CategoriesTable.php
+++ app/Filament/Resources/Categories/Tables/CategoriesTable.php
@@ -2,8 +2,6 @@
 
 namespace App\Filament\Resources\Categories\Tables;
 
-use Filament\Actions\BulkActionGroup;
-use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\IconColumn;
@@ -44,11 +42,6 @@ class CategoriesTable
             ->recordActions([
                 ViewAction::make(),
                 EditAction::make(),
-            ])
-            ->toolbarActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
-                ]),
             ]);
     }
 }
